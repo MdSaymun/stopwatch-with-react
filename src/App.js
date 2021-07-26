162b329bee5f515b82bbbe3d64ddd9739b75d214
MdSaymun <saymun2000@gmail.com>
2021-07-26 20:56:12 +0600

    change folder name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-// import TimerApp from "./Timer_State_practice/timeFunc";
-import PracTimer from "./Timer_State_practice/Practice";
+import TimerApp from "./Timer_State_practice/timeFunc";
 const App = () => {
   return (
     <div>
-      {/* <TimerApp /> */}
-      <PracTimer />
-      <h1 className="text-center bg-gray-700 py-1 px-2 text-gray-50">Lorem ipsum dolor sit.</h1>
+      <TimerApp />
     </div>
   );
 };
